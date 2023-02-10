84120e3aa615798af983f726e50fb988a4eee13b
Scalante <rubendarioscalante@gmail.com>
2023-02-09 23:01:17 -0500

    Se implementa el dashboard, se realizar validaciones en el login.

--- src/app/components/inicio/login/login.component.ts
+++ src/app/components/inicio/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Usuario } from '../../../models/usuario'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  login: FormGroup;
+
+  constructor(private fb: FormBuilder, 
+    private toastr: ToastrService, private router:Router) {
+    this.login = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  log(): void {
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
+    }
+
+    if (usuario.nombreUsuario === 'user' && usuario.password === 'admin') {
+      this.router.navigate(['/dashboard'])
+    }
+    else {
+      this.toastr.error('Usuario o contraseña incorrecto', 'Error')
+    }
+
+    console.log(usuario)
+  }
 
 }
--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  register: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.register = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ['']
+    },{validators: this.checkPassword});
+  }
+
+  ngOnInit(){
+
+  }
+
+  registrarUsuario(){
+    console.log(this.register)
+  }
+
+  checkPassword(group:FormGroup):any{
+    const pass = group.controls['password'].value;
+    const confirmPass = group.controls['confirmPassword'].value;
+    return pass === confirmPass ? null : {notSame:true}
+  }
+  
 
 }
